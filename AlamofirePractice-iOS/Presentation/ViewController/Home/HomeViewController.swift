d452b8557d8cca1d780539f063c706fb4a910fd2
sgmkhr <mksghr10@gmail.com>
2024-11-11 00:17:21 +0900

    MainStoryboard削除　起動時画面設定　ディレクトリ作成

---
+++ AlamofirePractice-iOS/Presentation/ViewController/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  AlamofirePractice-iOS
+//
+//  Created by 菅原実希 on 2024/11/11.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
